269156c694287402e81a532a7bdc12580865542d
jooyom <908joo@gamil.com>
2024-05-30 09:57:47 +0900

    init

---
+++ src/text2.java
@@ -0,0 +1,14 @@
+public class text2 {
+    /*public void printNum(int num){
+        System.out.println("숫자 출력:" + num);
+    }
+    public static void main(String[] args) {
+        VoidExample ex1 = new VoidExample();
+        ex1.printNum(5);
+    }*/
+    public int score(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+        return sum;
+    }
+}
