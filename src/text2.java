3a50b8c32757f1e249071d87701b1dd27fa75fe3
jooyom <908joo@gamil.com>
2024-05-30 10:06:47 +0900

    sdfa

--- src/text2.java
+++ src/text2.java
@@ -1,7 +1,7 @@
 public class text2 {
     /*public void printNum(int num){
         System.out.println("숫자 출력:" + num);
-    }
+    }@@@@#!@
     public static void main(String[] args) {
         VoidExample ex1 = new VoidExample();
         ex1.printNum(5);
